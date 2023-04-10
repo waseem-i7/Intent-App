0bcdd77f98064031ce33a0a4f833408ea17750b8
Waseem idrisi <waseem@greenlava.in>
2023-04-10 22:07:07 +0530

    10-04-2023

--- app/src/main/java/com/wizion/bitthdaygreetingapp/BirthdayGreetingActivity.java
+++ app/src/main/java/com/wizion/bitthdaygreetingapp/BirthdayGreetingActivity.java
@@ -3,12 +3,19 @@ package com.wizion.bitthdaygreetingapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class BirthdayGreetingActivity extends AppCompatActivity {
 
+    TextView tvName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_birthday_greeting);
+
+        String name = getIntent().getStringExtra(MainActivity.KEY);
+        tvName = findViewById(R.id.tvName);
+        tvName.setText(name);
+
     }
 }
--- app/src/main/java/com/wizion/bitthdaygreetingapp/MainActivity.java
+++ app/src/main/java/com/wizion/bitthdaygreetingapp/MainActivity.java
@@ -2,13 +2,42 @@ package com.wizion.bitthdaygreetingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaration of variables
+    EditText etName;
+    Button btnEnter;
+
+    static String KEY = "BirthdayPersonName";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialization
+        etName = findViewById(R.id.etName);
+        btnEnter = findViewById(R.id.btnEnter);
+
+
+        //defining intent for navigate to birthdayGreetingActivity
+        btnEnter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //get the text from edittext
+                String name = etName.getText().toString();
+
+                Intent intent = new Intent(MainActivity.this,BirthdayGreetingActivity.class);
+                intent.putExtra(MainActivity.KEY,name);
+                startActivity(intent);
+            }
+        });
+
     }
 }
